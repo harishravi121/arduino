2521e84b735cb2194c3ad930ffee8399843e2684
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2024-09-06 18:34:59 +0530

    Update LCDFUN.ino

--- LCDFUN.ino
+++ LCDFUN.ino
@@ -59,7 +59,8 @@ void loop() {
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 0);
-  int a=random(2);
+  int a;
+  a=int(random(2));
   if(a==0){
   lcd.print("Dr. Harish is Fun");}
   if(a==1){
