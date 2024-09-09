9d8190c47898d2e50c59ce24be0710cad1e4d5bf
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2024-09-09 08:34:51 +0530

    Update LCDFUN.ino

--- LCDFUN.ino
+++ LCDFUN.ino
@@ -1,43 +1,5 @@
 /*
-  LiquidCrystal Library - Hello World
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
-
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- https://docs.arduino.cc/learn/electronics/lcd-displays
-
+Code by Dr. Harish with LCD shield 
 */
 
 // include the library code:
@@ -45,7 +7,7 @@
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
@@ -60,15 +22,19 @@ void loop() {
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 0);
   int a;
-  a=int(random(2));
+   a=int(random(3));
   if(a==0){
-  lcd.print("Dr. Harish is Fun");}
+  lcd.print("Dr. Harish  Fun   ");}
   if(a==1){
-    lcd.print("God is great");
+    lcd.print("God is great    ");
+  }
+
+   if(a==2){
+    lcd.print("Om Sri Sairam   ");
   }
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
-  lcd.print(random(1000));
-  delay(random(1000));
+  lcd.print(1000+random(1000));
+  delay(1000+random(1000));
   
 }
