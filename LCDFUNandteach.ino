87ffbe66285f88f5e123270edbb0439decad74e7
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2024-09-09 09:08:46 +0530

    Add files via upload

---
+++ LCDFUNandteach.ino
@@ -0,0 +1,70 @@
+/*
+Code by Dr. Harish with LCD shield 
+*/
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 0);
+  int a;
+   a=int(random(3));
+  if(a==0){
+  lcd.print("Dr. Harish  Fun   ");}
+  if(a==1){
+    lcd.print("God is great    ");
+  }
+
+   if(a==2){
+    lcd.print("Om Sri Sairam   ");
+  }
+  lcd.setCursor(0, 1);
+  
+  
+  int b;
+  int c;
+  int d;
+   a=int(random(2));
+  if(a==0){//select sum
+  b=int(random(120));
+  c=int(random(120));
+  d=b+c;
+ 
+      lcd.print(b);
+    lcd.print(" + ");
+    lcd.print(c);
+    
+      lcd.print(" = ");
+      lcd.print(d);
+      lcd.print("    ");
+    }
+  if(a==1){//select product
+  b=int(random(20));
+  c=int(random(20));
+  d=b*c;
+ 
+      lcd.print(b);
+    lcd.print(" * ");
+    lcd.print(c);
+    
+      lcd.print(" = ");
+      lcd.print(d);
+      lcd.print("    ");
+    }
+  delay(3000+random(3000));
+  
+}
